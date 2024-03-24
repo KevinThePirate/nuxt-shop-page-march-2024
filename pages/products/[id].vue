038f184d0466754b2af5a8b3aad34551502bbea0
Kevin <smithkevin1100@gmail.com>
2024-03-24 11:15:09 +0000

    added FakeStoreApi

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -2,10 +2,16 @@
   <div>
     <p>Product Details for {{ id }}</p>
     <p>Product details content</p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 <script setup>
 const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+const { data: product } = await useFetch(uri, { key: id });
+
 definePageMeta({
   layout: "products",
 });
